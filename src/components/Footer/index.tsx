f0e1c2b415c1aac40fb9522812410532da62beb9
GinMiraing <ginmiraing@outlook.com>
2023-07-30 21:04:29 +0800

    feat: 创建 Footer 组件

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+export const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 border-t w-full bg-white">
+      <div className="flex flex-col md:flex-row justify-between p-4 md:p-8 h-full items-center">
+        <div className="text-sm">©️ 2023 Yin. All rights reserved.</div>
+        <div className="text-sm mt-2 md:mt-0">
+          {"Based on "}
+          <a href={"https://nextjs.org/"}>Next.js</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
