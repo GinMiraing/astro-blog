82972981a894b58bfd44be0f89eaa7c2aa45adb9
GinMiraing <ginmiraing@outlook.com>
2023-07-31 19:09:05 +0800

    style: 修改 Footer 样式

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,11 @@
 export const Footer = () => {
   return (
-    <footer className="fixed bottom-0 border-t w-full bg-white">
-      <div className="flex flex-col md:flex-row justify-between p-4 md:p-8 h-full items-center">
-        <div className="text-sm">©️ 2023 Yin. All rights reserved.</div>
-        <div className="text-sm mt-2 md:mt-0">
+    <footer className="border-t h-16 w-full bg-white">
+      <div className="flex flex-col md:flex-row justify-between p-3 md:p-8 h-full items-center">
+        <div className="text-xs md:text-sm">
+          ©️ 2023 Yin. All rights reserved.
+        </div>
+        <div className="text-xs md:text-sm">
           {"Based on "}
           <a href={"https://astro.build/"}>Astro</a>
         </div>
