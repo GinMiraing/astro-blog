ef4dc784cbcb6233614602126da6b21214e603fd
GinMiraing <ginmiraing@outlook.com>
2023-07-31 23:38:08 +0800

    style: 更新 PostCard 样式

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -7,7 +7,7 @@ type PostCardProps = {
 
 export const PostCard = ({ post }: PostCardProps) => {
   return (
-    <div className="flex flex-col border rounded-md overflow-hidden">
+    <div className="flex flex-col border-stone-300 hover:border-rose-300 border rounded-lg overflow-hidden">
       <div className="w-full h-48 overflow-hidden">
         <a href={`/p/${post.slug}`}>
           <img
@@ -19,19 +19,29 @@ export const PostCard = ({ post }: PostCardProps) => {
         </a>
       </div>
       <div className="px-4 my-3">
-        <h1 className="text-xl font-sans font-medium">
-          <a href={`/p/${post.slug}`}>{post.data.title}</a>
+        <h1 className="text-xl font-serif font-medium text-stone-900">
+          <a
+            className="hover:text-rose-400 transition-colors"
+            href={`/p/${post.slug}`}
+          >
+            {post.data.title}
+          </a>
         </h1>
-        <time className="font-serif mt-2">
+        <time className="block font-serif mt-2 text-stone-600">
           {dayjs(post.data.date).format("YYYY-MM-DD")}
         </time>
       </div>
       <div className="px-4 my-3 flex-grow">
-        <p className="text-justify font-serif line-clamp-2">
-          <a href={`/p/${post.slug}`}>{post.data.description}</a>
+        <p className="text-justify font-serif line-clamp-2 text-stone-900">
+          <a
+            className="hover:text-rose-400 transition-colors"
+            href={`/p/${post.slug}`}
+          >
+            {post.data.description}
+          </a>
         </p>
       </div>
-      <div className="px-4 mt-2 text-sm font-serif font-medium pb-4">{`# ${post.data.category}`}</div>
+      <div className="px-4 mt-2 text-sm font-serif text-stone-600 font-medium pb-4">{`# ${post.data.category}`}</div>
     </div>
   );
 };
