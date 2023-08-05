dba35cc3a30a5ca3e2fed1f396950a78adf80dc2
GinMiraing <ginmiraing@outlook.com>
2023-08-05 16:45:53 +0800

    feat: 完善文章页的设计

--- src/components/LazyImage/index.tsx
+++ src/components/LazyImage/index.tsx
@@ -9,29 +9,69 @@ type LazyImageProps = {
   className?: string;
 };
 
-const LazyImage = ({ src, alt, width, height, className }: LazyImageProps) => {
+export const LazyImage = ({
+  src,
+  alt,
+  width,
+  height,
+  className,
+}: LazyImageProps) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    rootMargin: "10px 0px",
+  });
+  return (
+    <div className="h-full w-full overflow-hidden">
+      <div
+        ref={ref}
+        data-inview={inView}
+        className="relative data-[inview=true]:blur-none blur-sm duration-700 transition-all h-full w-full"
+      >
+        <img
+          alt={alt}
+          src={
+            inView
+              ? `${src}@${width}w_${height}h_1c.webp`
+              : `https://article.biliimg.com/bfs/article/3ab219979329e88a15d8d38b7e67c0e283945f3d.png@${width}w_${height}h_1c.webp`
+          }
+          className={cn("object-cover h-full w-full", className)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export const LazyImageWithFancyBox = ({
+  src,
+  alt,
+  width,
+  height,
+  className,
+}: LazyImageProps) => {
   const { ref, inView } = useInView({
     triggerOnce: true,
     rootMargin: "10px 0px",
   });
 
   return (
-    <div
-      ref={ref}
-      data-inview={inView}
-      className="relative data-[inview=true]:blur-none blur-sm duration-700 transition-all h-full w-full overflow-hidden"
-    >
-      <img
-        alt={alt}
-        src={
-          inView
-            ? `${src}@${width}w_${height}h_1c.webp`
-            : `https://article.biliimg.com/bfs/article/3ab219979329e88a15d8d38b7e67c0e283945f3d.png@${width}w_${height}h_1c.webp`
-        }
-        className={cn("object-cover h-full w-full", className)}
-      />
+    <div className="h-full w-full overflow-hidden">
+      <a data-fancybox data-caption={alt} href={src}>
+        <div
+          ref={ref}
+          data-inview={inView}
+          className="relative data-[inview=true]:blur-none blur-sm duration-700 transition-all h-full w-full"
+        >
+          <img
+            alt={alt}
+            src={
+              inView
+                ? `${src}@${width}w_${height}h_1c.webp`
+                : `https://article.biliimg.com/bfs/article/3ab219979329e88a15d8d38b7e67c0e283945f3d.png@${width}w_${height}h_1c.webp`
+            }
+            className={cn("object-cover h-full w-full", className)}
+          />
+        </div>
+      </a>
     </div>
   );
 };
-
-export default LazyImage;
