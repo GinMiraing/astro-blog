de91e7f3ebf03bb9a664db37ae3fab818e2fcd93
GinMiraing <ginmiraing@outlook.com>
2023-08-16 23:55:39 +0800

    refactor: 删除分页功能

--- src/components/Issue/IssueList/index.tsx
+++ src/components/Issue/IssueList/index.tsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
 import { IssueCard } from "../IssueCard";
-import { cn } from "@/lib/utils";
 
 type Issue = {
   message: string;
@@ -12,39 +10,11 @@ type Issue = {
 };
 
 export const IssueList = ({ issues }: { issues: Issue[] }) => {
-  const [currentList, setCurrentList] = useState<Issue[]>([]);
-  const [offset, setOffset] = useState<number>(0);
-
-  useEffect(() => {
-    const issueStore = sessionStorage.getItem("issue");
-    setCurrentList(issues.slice(0, issueStore ? parseInt(issueStore) - 1 : 7));
-    setOffset(issueStore ? parseInt(issueStore) : 8);
-  }, []);
-
-  const btnClickHandler = () => {
-    sessionStorage.setItem("issue", (offset + 8).toString());
-    setOffset((prev) => {
-      return prev + 8;
-    });
-    setCurrentList((prev) => [...prev, ...issues.slice(offset, offset + 7)]);
-  };
   return (
-    <>
-      <div className="grid my-6 grid-cols-1 gap-4 divide-y">
-        {currentList.map((issue) => (
-          <IssueCard key={issue.id} issue={issue} />
-        ))}
-      </div>
-      <div
-        className={cn(
-          "flex hover:text-pink transition-colors h-12 justify-center",
-          { hidden: offset >= issues.length }
-        )}
-      >
-        <button className="text-sm" onClick={() => btnClickHandler()}>
-          - 加载更多 -
-        </button>
-      </div>
-    </>
+    <div className="grid my-6 grid-cols-1 gap-4 divide-y">
+      {issues.map((issue) => (
+        <IssueCard key={issue.id} issue={issue} />
+      ))}
+    </div>
   );
 };
--- src/components/Post/PostList/index.tsx
+++ src/components/Post/PostList/index.tsx
@@ -1,42 +1,12 @@
-import { useEffect, useState } from "react";
 import { PostCard } from "../PostCard";
-import { cn } from "@/lib/utils";
 import type { CollectionEntry } from "astro:content";
 
 export const PostList = ({ posts }: { posts: CollectionEntry<"blog">[] }) => {
-  const [currentList, setCurrentList] = useState<CollectionEntry<"blog">[]>([]);
-  const [offset, setOffset] = useState<number>(0);
-
-  useEffect(() => {
-    const postStore = sessionStorage.getItem("post");
-    setCurrentList(posts.slice(0, postStore ? parseInt(postStore) - 1 : 7));
-    setOffset(postStore ? parseInt(postStore) : 8);
-  }, []);
-
-  const btnClickHandler = () => {
-    sessionStorage.setItem("post", (offset + 8).toString());
-    setOffset((prev) => {
-      return prev + 8;
-    });
-    setCurrentList((prev) => [...prev, ...posts.slice(offset, offset + 7)]);
-  };
   return (
-    <>
-      <div className="grid grid-cols-1 divide-y">
-        {currentList.map((post) => {
-          return <PostCard key={post.id} post={post.data} slug={post.slug} />;
-        })}
-      </div>
-      <div
-        className={cn(
-          "flex hover:text-pink transition-colors h-12 justify-center",
-          { hidden: offset >= posts.length }
-        )}
-      >
-        <button className="text-sm" onClick={() => btnClickHandler()}>
-          - 加载更多 -
-        </button>
-      </div>
-    </>
+    <div className="grid grid-cols-1 divide-y">
+      {posts.map((post) => {
+        return <PostCard key={post.id} post={post.data} slug={post.slug} />;
+      })}
+    </div>
   );
 };
