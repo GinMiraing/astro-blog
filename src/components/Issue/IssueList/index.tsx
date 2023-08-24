9f64f0749c15a2b427202d511fe7f442f8babb41
GinMiraing <ginmiraing@outlook.com>
2023-08-24 23:26:27 +0800

    style: 修改说说列表样式

--- src/components/Issue/IssueList/index.tsx
+++ src/components/Issue/IssueList/index.tsx
@@ -11,7 +11,7 @@ type Issue = {
 
 export const IssueList = ({ issues }: { issues: Issue[] }) => {
   return (
-    <div className="grid my-6 grid-cols-1 gap-4 divide-y">
+    <div className="grid grid-cols-1 divide-y">
       {issues.map((issue) => (
         <IssueCard key={issue.id} issue={issue} />
       ))}
