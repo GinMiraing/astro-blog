ec0b126f6ff6310e65177a7adc0193c4b7de5a9e
GinMiraing <ginmiraing@outlook.com>
2023-10-15 15:27:18 +0800

    feat: 全局定义 twikoo 类型

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare module "@swup/fade-theme";
+
+declare interface Window {
+  twikoo: {
+    init: (options: { envId: string; el: string; lang: string }) => void;
+  };
+}
